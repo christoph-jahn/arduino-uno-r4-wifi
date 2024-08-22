15a2ecf6570404f47371cda95efdb4a58fb63bf6
Christoph Jahn <cjahn@posteo.de>
2024-08-22 19:03:19 +0200

    Slide 4 LEDs

--- Arduino_Uno_R4_Wifi_LESSON3/Arduino_Uno_R4_Wifi_LESSON3.ino
+++ Arduino_Uno_R4_Wifi_LESSON3/Arduino_Uno_R4_Wifi_LESSON3.ino
@@ -8,8 +8,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
-  digitalWrite(12, HIGH);
-  digitalWrite(11, HIGH);
-  digitalWrite(10, HIGH);
+  digitalWrite(13, HIGH); delay(200); digitalWrite(13, LOW);
+  digitalWrite(12, HIGH); delay(200); digitalWrite(12, LOW);
+  digitalWrite(11, HIGH); delay(200); digitalWrite(11, LOW);
+  digitalWrite(10, HIGH); delay(100); digitalWrite(10, LOW);
+  digitalWrite(11, HIGH); delay(100); digitalWrite(11, LOW); 
+  digitalWrite(12, HIGH); delay(100); digitalWrite(12, LOW);
 }
