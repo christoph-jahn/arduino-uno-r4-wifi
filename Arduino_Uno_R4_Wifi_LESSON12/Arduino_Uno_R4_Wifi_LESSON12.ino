b16539131faa3b68fd61c4935e7419234c906dec
Christoph Jahn <cjahn@posteo.de>
2024-08-23 16:38:38 +0200

    Add LED with brightness controlled by potentiometer

--- Arduino_Uno_R4_Wifi_LESSON12/Arduino_Uno_R4_Wifi_LESSON12.ino
+++ Arduino_Uno_R4_Wifi_LESSON12/Arduino_Uno_R4_Wifi_LESSON12.ino
@@ -2,34 +2,56 @@
  * Schaltplan mit Potentiometer:
  *   .---------.
  *   |         |GND    ___ 
- *   | Arduino |------|___|---.
- *   | Uno R4  |        A     |
- *   | Wifi    |A2      |     |
- *   |         |--------'     |
- *   |         |              |
- *   |         |5V            |
- *   |         |--------------'
- *   '---------'
+ *   | Arduino |------|___|----o----.
+ *   | Uno R4  |        A      |    |
+ *   | Wifi    |A2      |      |    |
+ *   |         |--------'      |    |
+ *   |         |               |    |
+ *   |         |5V             |    |
+ *   |         |---------------'    |
+ *   |         |                    |
+ *   |         |~11  //     ___     |
+ *   |         |----->|----|___|----'
+ *   '---------'             1k
  *
+ * Anmerkung: 
+ *   Sobald die LED angeschlossen ist, schwankt der ausgelesene Potentiometerwert +/- 20.
+ *   Mit einer Abtastrate von ca. 10x / Sekunde, keine Schwankungen zu beobachten.
  */
 
 const int ANALOG_POTENTIOMETER_PIN = A2;
+const int LED_PWM_PIN = 11;
+const int MAX_POTENTIOMETER = 1023;
 const int SERIAL_BAUD_RATE = 9600;
-const int WAIT_TIME = 1000;
+const int TOLERANCE = 1;
+const int POLLING_TIME = 100;
 
-int measurement; 
+int measurement;
 
 void setup() {
   Serial.begin(SERIAL_BAUD_RATE);
   pinMode(ANALOG_POTENTIOMETER_PIN, INPUT);
+  pinMode(LED_PWM_PIN, OUTPUT);
 }
 
 void loop() {
-  int currentMeasurement = analogRead(ANALOG_POTENTIOMETER_PIN);
-  if (abs(currentMeasurement - measurement) <= 1) return; else measurement = currentMeasurement;
+  delay(POLLING_TIME);
+  if (!updateMeasurement()) return;
 
   float voltage = 5.0 / 1023.0 * measurement;
   Serial.print("Messung an PIN " + String(ANALOG_POTENTIOMETER_PIN) + " ist " + String(measurement) + ". ");
-  Serial.println("Das entspricht " + String(voltage) + "V.");
-  delay(WAIT_TIME);
+  Serial.print("Das entspricht " + String(voltage) + "V. ");
+
+  int brightness = (int) (255.0 * measurement / MAX_POTENTIOMETER);
+  analogWrite(LED_PWM_PIN, brightness);
+  Serial.println("Helligkeit der LED am PWM PIN " + String(LED_PWM_PIN) + " ist " + String(brightness) + ". ");
+}
+
+bool updateMeasurement() {
+  int currentMeasurement = analogRead(ANALOG_POTENTIOMETER_PIN);
+  bool inputHasChanged = abs(currentMeasurement - measurement) > TOLERANCE
+    || (currentMeasurement < measurement && currentMeasurement <= TOLERANCE)
+    || (currentMeasurement > measurement && currentMeasurement >= MAX_POTENTIOMETER - TOLERANCE);
+  if (inputHasChanged) measurement = currentMeasurement; 
+  return inputHasChanged;
 }
