df0c42820157373d47f6d2a8e2386d07160e6833
Christoph Jahn <cjahn@posteo.de>
2024-08-25 16:47:33 +0200

    Control LED with button switch

--- Arduino_Uno_R4_Wifi_LESSON24/Arduino_Uno_R4_Wifi_LESSON24.ino
+++ Arduino_Uno_R4_Wifi_LESSON24/Arduino_Uno_R4_Wifi_LESSON24.ino
@@ -8,22 +8,27 @@
  * Der "Reset"-Taster auf der Arduino-Platine startet das Programm neu.
  *
  * Schaltplan:
- *   .---------.             
- *   |         |GND   ____
- *   | Arduino |-----|____|-----.
- *   | Uno R4  |       10k      |
- *   | Wifi    |2               |    
- *   |         |----------------o |
- *   |         |                  |=|
- *   |         |                o |
- *   |         |5V              |
- *   |         |----------------'
+ *   .---------.
+ *   |         |~10  //    ____
+ *   |         |----->|---|____|--.
+ *   |         |             1k   |
+ *   |         |                  |
+ *   |         |GND   ____        |
+ *   | Arduino |-----|____|-------o
+ *   | Uno R4  |       10k        |
+ *   | Wifi    |2                 |    
+ *   |         |------------------o |
+ *   |         |                    |=|
+ *   |         |                  o |
+ *   |         |5V                |
+ *   |         |------------------'
  *   '---------'           
  * 
  */
 
 
 const int BUTTON_PIN = 2;
+const int LED_PIN = 10;
 const int SERIAL_BAUD_RATE = 115200;
 
 const int PAUSE_DURATION = 100;
@@ -31,10 +36,12 @@ const int PAUSE_DURATION = 100;
 void setup() {
   Serial.begin(SERIAL_BAUD_RATE);
   pinMode(BUTTON_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
   int value = digitalRead(BUTTON_PIN);
   Serial.println("Gelesener Wert des digitalen PINs " + String(BUTTON_PIN) + " ist " + String(value));
+  if (value == 1) digitalWrite(LED_PIN, LOW); else digitalWrite(LED_PIN, HIGH);
   delay(PAUSE_DURATION);
 }
