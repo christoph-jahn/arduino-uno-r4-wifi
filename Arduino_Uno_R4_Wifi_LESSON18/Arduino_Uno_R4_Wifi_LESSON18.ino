772ddc016c7fd0f5686654ad77952a5d792b2783
Christoph Jahn <cjahn@posteo.de>
2024-08-25 11:31:24 +0200

    Add prompt in serial monitor for mixed colors for RGB LED

--- Arduino_Uno_R4_Wifi_LESSON18/Arduino_Uno_R4_Wifi_LESSON18.ino
+++ Arduino_Uno_R4_Wifi_LESSON18/Arduino_Uno_R4_Wifi_LESSON18.ino
@@ -1,4 +1,12 @@
 /**
+ * In der Arduino IDE:
+ *   - öffne den "Seriellen Monitor" (Symbol rechts oben)
+ *   - Konfiguration des seriellen Monitors sollte wie folgt sein
+ *     - "Kein Zeilenende"
+ *     - "115200 Baud"
+ * 
+ * Mit dem "Reset"-Taster auf der Arduino-Platine starte das Programm neu.
+ *
  * Schaltplan:
  *   __________________
  *  |      RGB LED     |
@@ -29,19 +37,41 @@
  * also: ~11, ~10, ~9, ~6, ~5 und ~3.
  */
 
+const int SERIAL_BAUD_RATE = 115200;
+const int POLLING_DELAY = 100;
+
 const int RED_LED_PIN = 11;
 const int GREEN_LED_PIN = 10;
 const int BLUE_LED_PIN = 9;
 
 void setup() {
+  Serial.begin(SERIAL_BAUD_RATE);
   pinMode(RED_LED_PIN, OUTPUT);
   pinMode(GREEN_LED_PIN, OUTPUT);
   pinMode(BLUE_LED_PIN, OUTPUT);
-
 }
 
 void loop() {
-  digitalWrite(RED_LED_PIN, HIGH);
-  digitalWrite(GREEN_LED_PIN, HIGH);
-  digitalWrite(BLUE_LED_PIN, HIGH);
+  Serial.print("Welche Farbe soll leuchten (rot, grün, blau, cyan, magenta, gelb, orange oder weiß): ");
+  while (Serial.available() == 0) delay(POLLING_DELAY);
+  String input = Serial.readString();
+  Serial.println(input);
+
+  // Farbtabelle verfügbar unter: https://htmlcolorcodes.com/colors/
+  input.toLowerCase();
+  if (input == "rot") setRGB(255, 0, 0);
+  else if (input == "grün") setRGB(0, 255, 0);
+  else if (input == "blau") setRGB(0, 0, 255);
+  else if (input == "cyan") setRGB(0, 255, 255);
+  else if (input == "magenta") setRGB(255, 0, 255);
+  else if (input == "gelb") setRGB(255, 255, 0);
+  else if (input == "orange") setRGB(255, 165, 0);
+  else if (input == "weiß" || input == "weiss") setRGB(255, 255, 255);
+  else Serial.println("Die Farbe " + input + " ist nicht bekannt.");
+}
+
+void setRGB(int red, int green, int blue) {
+  analogWrite(RED_LED_PIN, red);
+  analogWrite(GREEN_LED_PIN, green);
+  analogWrite(BLUE_LED_PIN, blue);
 }
